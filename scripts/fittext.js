46021e34b4007042f0928dbabd070f28a8eea8d2
Rudy van Sloten <54894409+rdvansloten@users.noreply.github.com>
2020-10-06 23:12:32 +0200

    Reworked css,html (#5)

--- scripts/fittext.js
+++ scripts/fittext.js
@@ -1,12 +1,8 @@
 /*!	
-* FitText.js 1.0 jQuery free version
-*
 * Copyright 2011, Dave Rupert http://daverupert.com 
 * Released under the WTFPL license 
 * http://sam.zoy.org/wtfpl/
 * Modified by Slawomir Kolodziej http://slawekk.info
-*
-* Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
 */
 (function(){
 
