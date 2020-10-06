41f46014d4027995c4a18711954220fb59409425
rdvansloten <rdvansloten@gmail.com>
2020-10-06 23:11:12 +0200

    Reworked css,html

---
+++ scripts/tooltip.js
@@ -0,0 +1,22 @@
+function iconToolTip(x) {
+
+    var tooltip;
+
+    if(x === "linkedin") {
+        tooltip = "LinkedIn";
+    }
+    else if(x === "github") {
+        tooltip = "GitHub";
+    }
+    else if(x === "medium") {
+        tooltip = "Medium";
+    }
+    else if(x === "email") {
+        tooltip = "Email";
+    }
+    else if(x === "void") {
+        tooltip = "&nbsp;";
+    }
+
+    document.getElementById("icon-tooltip").innerHTML = tooltip;
+}
